b35ca08136219ba2819b5b0f6a59256ea43fa04d
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-12-08 23:02:02 +0300

    airbnb clone

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get -y install nginx
+
+# Create necessary folders if they don't exist
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+
+# Create a fake HTML file for testing
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+# Create or recreate symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user and group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+nginx_config="/etc/nginx/sites-available/default"
+sudo sed -i '/^\tlocation \/ {/i\
+\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' "$nginx_config"
+
+# Restart Nginx
+sudo service nginx restart
+
+exit 0
