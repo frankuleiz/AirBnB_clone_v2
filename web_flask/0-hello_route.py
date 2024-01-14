f933297dada1dcd366fb9cfa5d81a0124e08610c
Frank <kamunyuz@gmail.com>
2024-01-14 15:41:25 +0300

    Hello Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from flask import Flask
+
+
+"""
+ The script start a simple web application
+"""
+app = Flask(__name__)
+"""The Flask app instance"""
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """ The home page"""
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
