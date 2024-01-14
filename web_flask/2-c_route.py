f442c30d4991518f4d32d2e21778dafead17df43
Frank <kamunyuz@gmail.com>
2024-01-14 16:21:13 +0300

    c page route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""The script starts a web flask application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+"""The flask app instance"""
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """The homepage"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_page(text):
+    """The c page route"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
